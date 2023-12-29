f2d5d711ac04c39a2c18a77a45f6a7a892e4ea34
Vaibhav Bhardwaj <vaibhavbhardwaj1230@gmail.com>
2023-12-29 16:58:04 +0200

    feat: mongodb middleware to encrypt password before saving it

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 export type UserType = {
     _id: string;
@@ -10,9 +11,16 @@ export type UserType = {
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    password: { type: String, require: true },
-    firstName: { type: String, require: true },
-    lastName: { type: String, require: true },
+    password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+});
+
+userSchema.pre("save", async function (next) {
+    if(this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 8);
+    }
+    next();
 });
 
 const User = mongoose.model<UserType>('User', userSchema);
